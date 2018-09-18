facea148acabb4ba68749d914349baf9b79f5fd7
Patrick Dale Rugayan <pdale.rugayan@gmail.com>
2018-09-19 01:07:33 +0800

    Offline Fonts

--- resources/views/layout/adminlte.blade.php
+++ resources/views/layout/adminlte.blade.php
@@ -31,7 +31,6 @@
     <!-- bootstrap wysihtml5 - text editor -->
     <link rel="stylesheet" href="{{ asset('css/bootstrap3-wysihtml5.min.css') }}">
     <!-- Google Font -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 
     <link href="{{ asset('css/jquery.dataTables.min.css') }}" rel="stylesheet" type="text/css">
 
