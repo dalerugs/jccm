2a7f5abebd0c1ed43c92d8e15af1420663d040da
Patrick Dale Rugayan <pdale.rugayan@gmail.com>
2018-09-26 23:27:31 +0800

    Update Bat File

--- JCCM.bat
+++ JCCM.bat
@@ -1,5 +1,6 @@
 title JCCM Management System
 START c:/xampp/xampp-control.exe
+taskkill /im xampp-control.exe
 netsh wlan set hostednetwork mode=allow ssid=jccm key=password keyUsage=persistent
 netsh wlan start hostednetwork
 for /f "tokens=14" %%a in ('ipconfig ^| findstr IPv4') do set _IPaddr=%%a
