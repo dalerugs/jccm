b84d9eef7a16355164d5fb4811e4d3172d8b2451
Patrick Dale Rugayan <pdale.rugayan@gmail.com>
2018-09-26 22:34:36 +0800

    Batch File

--- resources/views/layout/default.blade.php
+++ resources/views/layout/default.blade.php
@@ -6,8 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'JCCM') }} | {{$page_title}}</title>
-
-    <link href="https://fonts.googleapis.com/css?family=Abel|Open+Sans+Condensed:300" rel="stylesheet">
     <link rel="icon" href="{{ asset('img/fav1.jpg') }}" type="image/x-icon"/>
     <link rel="shortcut icon" href="{{ asset('img/fav1.jpg') }}" type="image/x-icon"/>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css">
